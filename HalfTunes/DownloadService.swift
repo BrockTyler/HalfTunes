c4c996d92fe61327ad61c64a23f1e3e0cad78e0b
BrockTyler <35366483+BrockTyler@users.noreply.github.com>
2018-02-07 15:01:16 -0700

    Created URLSessionDownloadDelegate and a Download task; user can now tap 'Download' on song list view to download the track preview.

--- HalfTunes/DownloadService.swift
+++ HalfTunes/DownloadService.swift
@@ -9,10 +9,26 @@ class DownloadService {
   // SearchViewController creates downloadsSession
   var downloadsSession: URLSession!
 
+  // This dict maintains mapping between a URL and its active Download, if any.
+  var activeDownloads: [URL: Download] = [:]
+  
   // MARK: - Download methods called by TrackCell delegate methods
 
   func startDownload(_ track: Track) {
-    // TODO
+    // 1. Initialize a Download with the track:
+    let download = Download(track: track)
+    
+    // 2. Using the new session object, create a URLSessionDownloadTask with track's preview URL, and set it to task property of the Download:
+    download.task = downloadsSession.downloadTask(with: track.previewURL)
+    
+    // 3. Start the download task by calling resume() on it:
+    download.task?.resume()
+    
+    // 4. Indicate that download is in progress:
+    download.isDownloading = true
+    
+    // 5. Map download URL to its Download in the activeDownloads dict:
+    activeDownloads[download.track.previewURL] = download
   }
   // TODO: previewURL is http://a902.phobos.apple.com/...
   // why doesn't ATS prevent this download?
